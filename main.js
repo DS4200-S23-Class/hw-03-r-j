60e517d5b3020fe9fd314b9a55c19390c2f126a3
jingyig16 <jingyig616@gmail.com>
2023-02-07 23:57:38 -0500

    updatessssssssss

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-function ShowAndHide(){
-  var div = document.getElementById("answer");
-  div.classList.toggle("all");
+function ShowAndHide() {
+  let buttonDiv = document.getElementById("answer");
+  let clsname = buttonDiv.className;
+
+  // show the answers when clicking the button with everything hidden
+  do {
+    clsname = 'show';
+    buttonDiv.classList.toggle(clsname);
 }
+  while (clsname == 'hide');
+
+  // hide the answers when clicking the button with everything shown
+  do {
+    clsname = 'hide';
+    buttonDiv.classList.toggle(clsname);
+  }
+  while (clsname == 'show');
+}
+
+// documentation:
+// by changing the classname in the div whose id is "answer", 
+// we can change the style of the div by changing their classname
+// to avoid use any styling done in js
