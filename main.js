90b8ffe38fb7d2276fae6ddfa9023f37bc57134e
jingyig16 <jingyig616@gmail.com>
2023-02-08 00:11:45 -0500

    stuff ..

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
 function ShowAndHide() {
   let buttonDiv = document.getElementById("answer");
-  let clsname = buttonDiv.className;
-
-  // show the answers when clicking the button with everything hidden
-  do {
-    clsname = 'show';
-    buttonDiv.classList.toggle(clsname);
-}
-  while (clsname == 'hide');
-
-  // hide the answers when clicking the button with everything shown
-  do {
-    clsname = 'hide';
-    buttonDiv.classList.toggle(clsname);
-  }
-  while (clsname == 'show');
+  buttonDiv.classList.toggle('hide');
 }
 
-// documentation:
-// by changing the classname in the div whose id is "answer", 
-// we can change the style of the div by changing their classname
-// to avoid use any styling done in js
+// we can hide all the graphs and texts by clicking the button
